3de60de7371ef0cf75ac55b3d22bd7e1ce7cb94d
Weslley Lima <weslleyclima@yahoo.com.br>
2021-08-29 22:52:07 -0300

    Ajustar modal AdBlock para Sucesso

--- js/modal.js
+++ js/modal.js
@@ -3,20 +3,6 @@ const detect = document.querySelector("#detect");
 const wrapper = document.querySelector(".wrapper");
 const button = wrapper.querySelector("button");
 
-let adClasses = ["ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"];
-
-for (let item of adClasses) {
-    detect.classList.add(item);
-}
-let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
-if (!wrapper.classList.contains("show")) {
-    if (getProperty == "none") {
-        wrapper.classList.add("show");
-    } else {
-        wrapper.classList.remove("show");
-    }
-    //getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
-}
 button.addEventListener("click", function() {
     wrapper.classList.remove("show");
     return window.history.go(-1);
